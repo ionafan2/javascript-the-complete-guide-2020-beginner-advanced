53538484d3e62d702dbd7800986435b02d618907
Pavlo Cherniavskyi <noreply@github.com>
2022-09-18 15:00:06 -0700

    39. Connecting all Buttons to Functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,51 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-let calc = `${defaultResult} + 10`;
-currentResult += 10;
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+divideBtn.addEventListener('click', divide);
+multiplyBtn.addEventListener('click', multiply);
 
 function getUserInput() {
   return parseInt(userInput.value);
 }
 
+function createAndWriteLog(operator, before, calcNumber) {
+  return `${before} ${operator} ${calcNumber}`;
+}
+Í;
 function add() {
-  const calcDesc = `${currentResult} + ${getUserInput()}`;
+  let initialRes = currentResult;
   currentResult += getUserInput();
+  outputResult(
+    currentResult,
+    createAndWriteLog('+', initialRes, getUserInput())
+  );
+}
 
-  outputResult(currentResult, calcDesc);
+function subtract() {
+  let initialRes = currentResult;
+  currentResult -= getUserInput();
+  outputResult(
+    currentResult,
+    createAndWriteLog('-', initialRes, getUserInput())
+  );
+}
+
+function divide() {
+  let initialRes = currentResult;
+  currentResult /= getUserInput();
+  outputResult(
+    currentResult,
+    createAndWriteLog('/', initialRes, getUserInput())
+  );
+}
+
+function multiply() {
+  let initialRes = currentResult;
+  currentResult *= getUserInput();
+  outputResult(
+    currentResult,
+    createAndWriteLog('/', initialRes, getUserInput())
+  );
 }
