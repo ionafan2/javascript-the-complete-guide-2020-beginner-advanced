e32a95125e2d90e777c9e1dd11bbe3ad34e8d3f7
Developer <dev.privacy719@slmails.com>
2025-10-26 17:43:49 -0700

    48. Adding a Re-Usable Function That Uses Objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,35 +11,41 @@ function createAndWriteLog(operator, before, calcNumber) {
     return `${before} ${operator} ${calcNumber}`;
 }
 
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     let initialRes = currentResult;
     currentResult += parseInt(userInput.value);
     outputResult(currentResult, createAndWriteLog('+', initialRes, parseInt(userInput.value)));
-    let logEntry = {
-        operation: 'ADD',
-        prevResult: initialRes,
-        number: parseInt(userInput.value),
-        result: currentResult
-    }
-    console.log(logEntry.operation);
-    logEntries.push(logEntry);
-    console.log(logEntries);
+    writeToLog('ADD', initialRes, parseInt(userInput.value), currentResult);
 }
 
 function subtract() {
     let initialRes = currentResult;
     currentResult -= parseInt(userInput.value);
     outputResult(currentResult, createAndWriteLog('-', initialRes, parseInt(userInput.value)));
+    writeToLog('SUBTRACT', initialRes, parseInt(userInput.value), currentResult);
 }
 
 function divide() {
     let initialRes = currentResult;
     currentResult /= parseInt(userInput.value);
     outputResult(currentResult, createAndWriteLog('/', initialRes, parseInt(userInput.value)));
+    writeToLog('DIVIDE', initialRes, parseInt(userInput.value), currentResult);
 }
 
 function multiply() {
     let initialRes = currentResult;
     currentResult *= parseInt(userInput.value);
     outputResult(currentResult, createAndWriteLog('/', initialRes, parseInt(userInput.value)));
+    writeToLog('MULTIPLY', initialRes, parseInt(userInput.value), currentResult);
 }
